597886c6aec57801943c5b5030a7429528a0babe
ContinuumLS <231798356+ContinuumLS@users.noreply.github.com>
2026-06-04 18:32:36 -0400

    Add Docker support

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+echo '(1) Starting virtual TTY bridge...'
+socat PTY,link=/dev/ttyACM0,raw,echo=0 TCP:host.docker.internal:${DEVICE_PORT} &
+
+echo '(2) Waiting for virtual device...'
+while [ ! -e /dev/ttyACM0 ]; do
+    sleep 0.5
+done
+
+echo 'READY!'
+echo '   To edit config: nano config.toml'
+echo '   To run mag-usb: ./mag-usb'
+exec bash
